e763581f14251e9841f5c962b2927babb5d15ce7
Renat Gubaev <gubaev.r@n-t.io>
2024-09-16 14:11:47 +0300

    no message

--- src/packages/application/api/build/src/packages/application/api/ormconfig.migration.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppSettings_1 = require("./src/AppSettings");
-const AppModule_1 = require("./src/AppModule");
-const typeorm_1 = require("typeorm");
-exports.default = new typeorm_1.DataSource(AppModule_1.AppModule.getOrmConfig(new AppSettings_1.AppSettings())[0]);
